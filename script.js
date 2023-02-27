ee578b121ac55253aa67ca169ab2ee1c3f5ae6e0
Prakash <prakashakrakr@gmail.com>
2023-02-27 13:33:46 +0530

    Done

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  menuIcon.classList.toggle("active");
+};
+
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
-    let top = window.screenY;
+    let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
@@ -18,9 +26,35 @@ window.onscroll = () => {
     }
   });
 
-  let header = document.querySelector('header');
+  let header = document.querySelector("header");
 
-  header.classList.toggle('sticky', window.screenY > 100)
+  header.classList.toggle("sticky", window.screenY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 1500,
+  delay: 200,
+});
 
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+// Multiple Text Typed Js
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer, Software Engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
